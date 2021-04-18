603f51a9d25221ddcbdcf46d2cdda9a4b4c7aed3
Yair Haimovitch <yairh@wix.com>
2021-04-18 23:56:06 +0300

    remove cache config

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
-const os = require('os');
-const path = require('path');
-const { log } = require('./utils/logger');
-const { getRepoName } = require('./utils/git');
+// const os = require('os');
+// const path = require('path');
+// const { log } = require('./utils/logger');
+// const { getRepoName } = require('./utils/git');
 
-const tmpDir = os.tmpdir();
-const repoName = getRepoName();
-const tmpPath = path.join(tmpDir, repoName || 'no-name');
-const cacheDir = path.join(tmpPath, '.stacker');
-const cacheFile = path.join(cacheDir, 'cache.json');
+// const tmpDir = os.tmpdir();
+// const repoName = getRepoName();
+// const tmpPath = path.join(tmpDir, repoName || 'no-name');
+// const cacheDir = path.join(tmpPath, '.stacker');
+// const cacheFile = path.join(cacheDir, 'cache.json');
 
-log(`cache file is located at ${cacheFile}`);
+// log(`cache file is located at ${cacheFile}`);
 
 module.exports = {
-	cacheDir,
-	cacheFile,
+	cacheDir: '',
+	cacheFile: '',
 };
